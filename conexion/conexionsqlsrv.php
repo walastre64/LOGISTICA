dc8a86a7120d917303ef9b112cb1368b1ed4cc17
Wilmar Alastre <51450021+walastre64@users.noreply.github.com>
2025-09-17 15:01:38 -0400

    Update conexionsqlsrv.php

--- conexion/conexionsqlsrv.php
+++ conexion/conexionsqlsrv.php
@@ -1,17 +1,17 @@
 <?PHP
 function conectate(){
 
-		$serverName = "DCBAHIA\MSSQLSERVER2014"; //serverName\instanceName
+		$serverName = "localhost"; //serverName\instanceName
 		$connectionInfo = array( "Database"		=>"BahiaRRHH",
 								 "UID"	   		=>"sa",
-								 "PWD"	   		=>"Ec14312183.-",
+								 "PWD"	   		=>"",
 								 "CharacterSet" =>"UTF-8",
 								 "TrustServerCertificate" => "yes"
 								 );
 		$conn = sqlsrv_connect( $serverName, $connectionInfo);
 	
 		if( $conn  == false) {
-			 echo "Conexión no se pudo establecer.<br />";
+			 echo "ConexiÃ³n no se pudo establecer.<br />";
 			 die( print_r( sqlsrv_errors(), true));
 		}else{
 			return $conn;
